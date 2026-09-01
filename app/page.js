f5829e7072c1c83cf6eb2bee4e45e0e631c47688
Walid Mahmoudi <145464003+Walid-Mahmoudi@users.noreply.github.com>
2026-09-02 00:06:42 +0300

    Remove UI overlay components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 import SalesAppClean from './SalesAppClean';
-import InboxNav from './InboxNav';
-import DashboardDataFix from './DashboardDataFix';
 
 export default function Page(){
-  return <><SalesAppClean/><InboxNav/><DashboardDataFix/></>;
+  return <SalesAppClean/>;
 }
