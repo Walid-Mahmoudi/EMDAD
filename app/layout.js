4c89e7ab72a2268706cb146f682a596c471e374b
Walid Mahmoudi <145464003+Walid-Mahmoudi@users.noreply.github.com>
2026-09-02 00:45:47 +0300

    Activate functional filter styles

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ import './collections-ux.css';
 import './companies-ux.css';
 import './navigation-ux.css';
 import './crm-enhancements.css';
+import './filters-ux.css';
 import CRMEnhancements from './CRMEnhancements';
 export const metadata={title:'EMDAD CRM',description:'EMDAD sales CRM workspace'};
 export default function RootLayout({children}){return <html lang="en"><body>{children}<CRMEnhancements/></body></html>}
