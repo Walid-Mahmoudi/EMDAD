f9742d94110d740dee2e0ddf9e14056dfb4150a1
Walid Mahmoudi <145464003+Walid-Mahmoudi@users.noreply.github.com>
2026-09-02 00:00:49 +0300

    Fix unresponsive CRM page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
 import SalesAppClean from './SalesAppClean';
 import InboxNav from './InboxNav';
 import DashboardDataFix from './DashboardDataFix';
-import CRMStructure from './crm-structure';
-import './crm-structure.css';
 
 export default function Page(){
-  return <><SalesAppClean/><InboxNav/><DashboardDataFix/><CRMStructure/></>;
+  return <><SalesAppClean/><InboxNav/><DashboardDataFix/></>;
 }
