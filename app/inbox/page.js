d4ea5a699a5c7310f7546f47d85f9c4789682c18
Walid Mahmoudi <145464003+Walid-Mahmoudi@users.noreply.github.com>
2026-09-01 12:23:04 +0300

    Wire AI extraction panel into Sales Inbox

--- app/inbox/page.js
+++ app/inbox/page.js
@@ -1,7 +1,8 @@
 'use client';
 import SalesInbox from '../SalesInbox';
 import EmailSyncPanel from './EmailSyncPanel';
+import AIAnalyzePanel from './AIAnalyzePanel';
 
 export default function InboxPage(){
-  return <><EmailSyncPanel/><SalesInbox/></>;
+  return <><EmailSyncPanel/><AIAnalyzePanel/><SalesInbox/></>;
 }
