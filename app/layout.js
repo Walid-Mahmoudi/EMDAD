54c7d4a4332c592ac5186a0b86df40f4db3c982e
Walid Mahmoudi <145464003+Walid-Mahmoudi@users.noreply.github.com>
2026-09-02 00:37:00 +0300

    Activate companies screen refinement

--- app/layout.js
+++ app/layout.js
@@ -7,5 +7,6 @@ import './pipeline-ux.css';
 import './projects-ux.css';
 import './activities-ux.css';
 import './collections-ux.css';
+import './companies-ux.css';
 export const metadata={title:'EMDAD CRM',description:'EMDAD sales CRM workspace'};
 export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
